b6ee49613eac8507defba6cf91e3c4496bbffda5
3CBuy <sentimentalwind@163.com>
2017-06-06 15:27:27 +0800

    第六次修改

--- 3CBuy/WebContent/jsp/Login.jsp
+++ 3CBuy/WebContent/jsp/Login.jsp
@@ -8,13 +8,13 @@
 </head>
 <center>
 <body>
-<form action="servlet/demo" method="post">
+<form action="<%=request.getContextPath()%>/DoLogin" method="POST">
 <fieldset>
 <legend>登录</legend>
  用户名:<input type="text" name="name" size="20" maxlength="18" placeholder="用户名" id="name" required="true"><br><br>
       密码:<input type="password" name="password" id="password" size="30" maxlength="25" placeholder="密码"onblur="pwdComplex()" onkeyup="pwdComplex()" required="true"><br><br>
-       <input type="button" value="登录">
-        <input type="button" value="注册">
+       <input type="submit" value="登录">
+        <input type="button" value="注册" onclick=window.location.href("regisiter.jsp")>      <! 跳转到注册页面去>
    </fieldset>
   </form>
 </body>
--- 3CBuy/src/servlet/DoLogin.java
+++ 3CBuy/src/servlet/DoLogin.java
@@ -6,19 +6,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import entity.UserInfo;
+import entity.LoginUserInfo;
 
 /**
- * Servlet implementation class demo
+ * Servlet implementation class DoLogin
  */
-@WebServlet("/demo")
-public class demo extends HttpServlet {
+@WebServlet("/DoLogin")
+public class DoLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public demo() {
+    public DoLogin() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -37,38 +37,23 @@ public class demo extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("utf-8");
-		
-		UserInfo u=new UserInfo();
-		String name;
-		String password;
-		String email;
-		String number;
-		
-		
-		
-		try
-		{
-			
-			name=request.getParameter("name");
-			password=request.getParameter("password");
-			email=request.getParameter("email");
-			number=request.getParameter("number");
-			//获取多个值
-			u.setName(name);
-			u.setPassword(password);
-			u.setEmail(email);
-			u.setNumber(number);
-			request.getSession().setAttribute("regUsers", u);//把用户信息保存在session中，取个名字叫regUsers
-			request.getRequestDispatcher("jsp/RegSuccess.jsp").forward(request, response);//请求转发提交给注册成功返回给用户
-			
-		}
-		catch(Exception ex)
-		{
-			
-			System.out.println("什么？出错了！");
-			ex.printStackTrace();
-			
-		}
+	    String username;
+	    String password;
+	    LoginUserInfo u=new LoginUserInfo();
+	    try
+	    {
+	    	username=request.getParameter("name");
+	    	password=request.getParameter("password");
+	    	u.setUsername(username);
+	    	u.setPassword(password);
+	    	//request.getSession().setAttribute("loginS", u);//把用户信息保存在session中，取个名字叫loginS
+			request.getRequestDispatcher("jsp/MainPage.jsp").forward(request, response);//请求转发提交给登录成功返回给用户
+	    	
+	    }
+	    catch(Exception ex)
+	    {   System.out.println("什么？！出错了！");
+	    	ex.printStackTrace();
+	    }
 	}
 
 }
